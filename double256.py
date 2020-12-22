978b1301f7909f68672f6789393dbc03c93f8590
TM <38635583+tm31420@users.noreply.github.com>
2020-12-22 18:42:03 -0500

    Update double256.py

--- double256.py
+++ double256.py
@@ -1,13 +1,10 @@
 
 import binascii, hashlib, base58, sys, ecdsa, codecs
 fly1 = open('double256s.txt', 'w')
-#Header order info
 
 
 def convertstring(aa):
-    # Step 1: get the privatekey in extended format, this is hexadecimal upper or lower case.
     string = aa
-    # Step 2: adding 80 in the front for select de MAINNET channel bitcoin address
     aa = (hashlib.sha256(hashlib.sha256(string).digest()).hexdigest())
     fly1.write("%s \n" % aa)    
     print aa
